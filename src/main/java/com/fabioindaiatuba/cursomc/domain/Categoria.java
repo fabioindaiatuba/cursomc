e1e9b176767b76abaa841dcfec32fb66e819cbb2
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2017-10-14 10:46:30 -0300

    Banco de Dados H2 e criaçao da base

--- src/main/java/com/fabioindaiatuba/cursomc/domain/Categoria.java
+++ src/main/java/com/fabioindaiatuba/cursomc/domain/Categoria.java
@@ -2,9 +2,18 @@ package com.fabioindaiatuba.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+//@Table(name="categoria")
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String nome;
