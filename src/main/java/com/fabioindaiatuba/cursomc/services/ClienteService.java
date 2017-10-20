631877e3fc621bc2e6d786fe126f887a306755e6
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2017-10-20 09:34:08 -0200

    PUT, DELETE, GET para Cliente

--- src/main/java/com/fabioindaiatuba/cursomc/services/ClienteService.java
+++ src/main/java/com/fabioindaiatuba/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.fabioindaiatuba.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fabioindaiatuba.cursomc.domain.Cliente;
+import com.fabioindaiatuba.cursomc.dto.ClienteDTO;
 import com.fabioindaiatuba.cursomc.repositories.ClienteRepository;
+import com.fabioindaiatuba.cursomc.services.exceptions.DataIntegrityException;
 import com.fabioindaiatuba.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,5 +28,41 @@ public class ClienteService {
 					+ ", Tipo: "+ Cliente.class.getName());
 		}
 		return obj;
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir um cliente com pedidos!");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, 
+			String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
 	}
 }
