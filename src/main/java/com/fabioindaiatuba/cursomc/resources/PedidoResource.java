5e27377884320e740fe2ad82f177332f13855a32
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2017-10-24 22:47:44 -0200

    Criando profile de teste

--- src/main/java/com/fabioindaiatuba/cursomc/resources/PedidoResource.java
+++ src/main/java/com/fabioindaiatuba/cursomc/resources/PedidoResource.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.fabioindaiatuba.cursomc.domain.Categoria;
 import com.fabioindaiatuba.cursomc.domain.Pedido;
-import com.fabioindaiatuba.cursomc.dto.CategoriaDTO;
 import com.fabioindaiatuba.cursomc.services.PedidoService;
 
 @RestController
