37e51e652a8e44c176cd77402faa2459d285349a
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2017-11-03 17:18:39 -0200

    Implementado esqueci a senha

--- src/main/java/com/fabioindaiatuba/cursomc/services/EmailService.java
+++ src/main/java/com/fabioindaiatuba/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.fabioindaiatuba.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.fabioindaiatuba.cursomc.domain.Cliente;
 import com.fabioindaiatuba.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,5 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 }
